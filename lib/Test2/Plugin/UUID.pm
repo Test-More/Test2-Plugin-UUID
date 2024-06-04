d70f8389eafc2ee543df34831c743759f42a9411
Chad Granum <exodist7@gmail.com>
2024-06-03 18:37:12 -0700

    Add uuid <-> bin converters

--- lib/Test2/Plugin/UUID.pm
+++ lib/Test2/Plugin/UUID.pm
@@ -19,13 +19,13 @@ sub import {
 sub apply_plugin {
     my $class = shift;
 
-    my ($gen_uuid, $backend) = Test2::Util::UUID->get_gen_uuid(@_);
+    my $subs = Test2::Util::UUID->get_gen_uuid(@_);
 
-    test2_add_uuid_via($gen_uuid);
+    test2_add_uuid_via($subs->{gen_uuid});
     require Test2::Hub;
     Test2::Hub->new; # Make sure the UUID generator is found
 
-    return $backend->();
+    return $subs->{GEN_UUID_BACKEND}->();
 }
 
 1;
